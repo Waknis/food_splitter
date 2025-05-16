2b4911944809cdb96f02d5c827b49d77dcd0bcdc
Mihir Waknis <mihirwaknis282@gmail.com>
2025-05-15 23:39:44 -0700

    add requirements.txt; prep for deployment

--- app.py
+++ app.py
@@ -93,4 +93,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Development server
+    app.run(host='0.0.0.0', port=8080, debug=True)
